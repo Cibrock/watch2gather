35c3197599bb855ec5c804c1e49bb8d12b40d229
Cibrock <jannis.salzmann@gmail.com>
2022-07-09 13:21:14 +0200

    Updates

---
+++ precache-manifest.2c5f9a8c5af534f90be34f230698188a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40c492a4225f2313a76d",
+    "url": "/watch2gather/static/css/main.03715f34.chunk.css"
+  },
+  {
+    "revision": "40c492a4225f2313a76d",
+    "url": "/watch2gather/static/js/main.40c492a4.chunk.js"
+  },
+  {
+    "revision": "cf3417458a39dce0c8ef",
+    "url": "/watch2gather/static/js/1.cf341745.chunk.js"
+  },
+  {
+    "revision": "51ef4534b2b8f73edda4",
+    "url": "/watch2gather/static/js/runtime~main.51ef4534.js"
+  },
+  {
+    "revision": "860475ab79ad5cef3b25c011a15d1b7f",
+    "url": "/watch2gather/static/media/bgvid.860475ab.mp4"
+  },
+  {
+    "revision": "b4a1df6dc9e0226ccb07246923b2bab2",
+    "url": "/watch2gather/index.html"
+  }
+];
