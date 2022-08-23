e6ef524e08105712a74adb23d39a77d8e1372977
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-08-23 18:19:14 +0200

    minor formatting

--- streamwithme/src/components/Navbar.js
+++ streamwithme/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import "./styles/navbar.css"
-import {Link} from "react-router-dom" ;
+import "./styles/navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return ( 
+    return (
         <nav className='sum'>
             <div className='logo'>
                 <Link to="/">StreamWithMe</Link>
             </div>
         </nav>
-        )
-}
-export default Navbar
+    );
+};
+export default Navbar;
