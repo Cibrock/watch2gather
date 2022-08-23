b50018f9ac7d3801ab23378491e1b94afd8624ba
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-08-23 18:37:41 +0200

    Updates

---
+++ precache-manifest.d6d06b264f50f55aaedceb2a81d95fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a642ecf698c09ae0a5a",
+    "url": "/watch2gather/static/css/main.88b057c2.chunk.css"
+  },
+  {
+    "revision": "1a642ecf698c09ae0a5a",
+    "url": "/watch2gather/static/js/main.1a642ecf.chunk.js"
+  },
+  {
+    "revision": "b8d8b3ff1f80d2429b7f",
+    "url": "/watch2gather/static/css/1.1d69f8d9.chunk.css"
+  },
+  {
+    "revision": "b8d8b3ff1f80d2429b7f",
+    "url": "/watch2gather/static/js/1.b8d8b3ff.chunk.js"
+  },
+  {
+    "revision": "51ef4534b2b8f73edda4",
+    "url": "/watch2gather/static/js/runtime~main.51ef4534.js"
+  },
+  {
+    "revision": "860475ab79ad5cef3b25c011a15d1b7f",
+    "url": "/watch2gather/static/media/bgvid.860475ab.860475ab.mp4"
+  },
+  {
+    "revision": "f61fbac09e34f7a417b424a50b7e065a",
+    "url": "/watch2gather/index.html"
+  }
+];
