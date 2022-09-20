3d1582bbcdf8190e47971ce48fa3f4306577af97
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-09-20 13:05:09 +0200

    Link in Room

--- streamwithme/src/components/Navbar.js
+++ streamwithme/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./styles/navbar.css";
 import { hookstate, useHookstate } from '@hookstate/core';
@@ -9,9 +9,13 @@ export const titleState = hookstate("");
 
 const Navbar = () => {
     useHookstate(titleState);
+    let linkIsVis = false;
     const room = titleState.get();
+    const [title,setTitle] = useState(room);
+
     const navigate = useNavigate();
     const navigateToHome = useCallback(() => navigate("/", { replace: true }), [navigate] );
+
     const handleKeyDown = (e) => { if (e.key === "Enter") handleNavigate(); }
     const handleNavigate = () => {
         if (room !== false) {
@@ -20,11 +24,18 @@ const Navbar = () => {
         }
         navigateToHome();
     }
+
     const toggleHelp = () => {
         // TODO
     }
+
     const copyLink = () => {
-        // TODO
+        if (!linkIsVis){
+            setTitle("https://cibrock.github.io/watch2gather/"+room);
+            navigator.clipboard.writeText("https://cibrock.github.io/watch2gather/"+room);
+            setTimeout(()=>{ setTitle(room); linkIsVis = false; },10000)
+            linkIsVis = true;
+        }
     }
 
     return (
@@ -33,7 +44,7 @@ const Navbar = () => {
                 <p tabIndex="0" className="logo-link" onClick={handleNavigate} onKeyDown={handleKeyDown} >StreamWithMe</p>
             </div>
             <div className='logo'>
-                <p className='logo-link' tabIndex="0" onClick={copyLink}>{room}</p>
+                <p className='logo-link' tabIndex="0" onClick={copyLink}>{title}</p>
             </div>
             <div className='logo'>
                 <p className="logo-link" tabIndex="0" onClick={toggleHelp}>Help</p>
