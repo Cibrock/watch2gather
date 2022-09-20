98e3566f6bb35683b06eaf3db2baf7380d9fed07
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-09-20 13:38:03 +0200

    Title with username

--- streamwithme/src/components/Navbar.js
+++ streamwithme/src/components/Navbar.js
@@ -1,25 +1,43 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./styles/navbar.css";
 import { hookstate, useHookstate } from '@hookstate/core';
 import { leaveRoom } from './API/RoomAPI';
 import { userState } from '../App';
+import { getUsers } from './API/UserAPI';
 
-export const titleState = hookstate("");
+export const roomTitleState = hookstate("");
 
 const Navbar = () => {
-    useHookstate(titleState);
+    useHookstate(roomTitleState);
     let linkIsVis = false;
-    const room = titleState.get();
-    const [title,setTitle] = useState(room);
+    const room = roomTitleState.get();
+    const [title,setTitle] = useState("StreamWithMe");
+    const [roomTitle,setRoomTitle] = useState(room);
 
     const navigate = useNavigate();
     const navigateToHome = useCallback(() => navigate("/", { replace: true }), [navigate] );
 
+    useEffect(()=>{checkTitle()});
+
+    const checkTitle = async () => {
+        if (userState.get()) {
+            const data = await getUsers();
+            const users = data.users;
+            if(users===undefined){
+                setTitle("StreamWithMe")
+                return;
+            }
+            const u = users.find((u)=> userState.get()===u.id)
+            setTitle("StreamWith"+u.name);
+        }
+        else setTitle("StreamWithMe");
+    }
+
     const handleKeyDown = (e) => { if (e.key === "Enter") handleNavigate(); }
     const handleNavigate = () => {
         if (room !== false) {
-            titleState.set(false);
+            roomTitleState.set(false);
             leaveRoom(room, userState.get());
         }
         navigateToHome();
@@ -31,9 +49,9 @@ const Navbar = () => {
 
     const copyLink = () => {
         if (!linkIsVis){
-            setTitle("https://cibrock.github.io/watch2gather/"+room);
-            navigator.clipboard.writeText("https://cibrock.github.io/watch2gather/"+room);
-            setTimeout(()=>{ setTitle(room); linkIsVis = false; },10000)
+            setRoomTitle("https://cibrock.github.io/watch2gather/#/"+room);
+            navigator.clipboard.writeText("https://cibrock.github.io/watch2gather/#/"+room);
+            setTimeout(()=>{ setRoomTitle(room); linkIsVis = false; },10000)
             linkIsVis = true;
         }
     }
@@ -41,10 +59,10 @@ const Navbar = () => {
     return (
         <div className='sum'>
             <div className='logo'>
-                <p tabIndex="0" className="logo-link" onClick={handleNavigate} onKeyDown={handleKeyDown} >StreamWithMe</p>
+                <p tabIndex="0" className="logo-link" onClick={handleNavigate} onKeyDown={handleKeyDown} >{title}</p>
             </div>
             <div className='logo'>
-                <p className='logo-link' tabIndex="0" onClick={copyLink}>{title}</p>
+                <p className='logo-link' tabIndex="0" onClick={copyLink}>{roomTitle}</p>
             </div>
             <div className='logo'>
                 <p className="logo-link" tabIndex="0" onClick={toggleHelp}>Help</p>
