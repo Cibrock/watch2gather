dad96da5322bcd1d2dd7545d5ae812e9a92f01d2
Cibrock <105553520+Cibrock@users.noreply.github.com>
2022-09-25 13:55:31 +0200

    not-found page

--- streamwithme/src/NotFound.js
+++ streamwithme/src/NotFound.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, {useCallback} from "react";
+import { useNavigate } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import "./components/styles/NotFound.css"
 
 const NotFound = () => {
     const TITLE = "StreamWithMe - Room not found";
 
+    const navigate = useNavigate();
+    const navigateToHome = useCallback(() => navigate("/", { replace: true }), [navigate]);
     return (
         <div className="App">
             <Helmet>
                 <title>{TITLE}</title>
             </Helmet>
             <Navbar />
-            <div>Dieser Raum existiert nicht mehr. Zurück zur Startseite</div>
+            <div className="not-found-flex"> 
+                <h1 className="not-found-header">Page Not Found</h1>
+                <div className="not-found-flex-row">                
+                    <p className="not-found-text">Dieser Raum existiert nicht mehr. Zurück zur Startseite.</p>
+                    <button className="not-found-btn" onClick={navigateToHome} aria-label="zurück zur Startseite">➔</button>
+                </div>
+            </div>
             <Footer />
         </div>
     );
